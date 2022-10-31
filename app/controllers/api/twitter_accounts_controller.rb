938256e05b845babc9aeeb5f32d05e6afba1a3a2
Roy <53046899+1997roylee@users.noreply.github.com>
2022-10-31 13:14:28 +0800

    add create method in twitter accounts controller

--- app/controllers/api/twitter_accounts_controller.rb
+++ app/controllers/api/twitter_accounts_controller.rb
@@ -1,2 +1,26 @@
-class Api::TwitterAccountsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  class TwitterAccountsController < ApplicationController
+    ## create a new twitter account
+    ## post /api/twitter_accounts
+    ## post /api/twitter_accounts.json
+    def create
+      @twitter_account = TwitterAccount.new(create_twitter_accounts_params)
+      if @twitter_account.save
+        render json: @twitter_account, status: :created
+      else
+        render json: @twitter_account.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def create_twitter_accounts_params
+      params.permit(
+        :username,
+        :name
+      )
+    end
+  end
 end
